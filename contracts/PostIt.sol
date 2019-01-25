9371b64ab4bd1c8a901357102f548b706c0d2463
Robert Van Sant <rvansant@Roberts-MacBook-Pro.local>
2019-01-25 00:52:10 -0500

    Adding comments to PostIt.sol smart contract.

--- contracts/PostIt.sol
+++ contracts/PostIt.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.5.0;
 
-
+/** @title PostIt. */
 contract PostIt {
+/**
+  * Variable declarations
+  */
 
   mapping (address => bool) public enrolled;
   mapping (address => User) public userProfiles;
@@ -19,6 +22,7 @@ contract PostIt {
     address payable accountAddress;
   }
 
+// struct Post
   struct Post {
     string title;
     string content;
@@ -30,6 +34,7 @@ contract PostIt {
     mapping (uint256 => Comment) comments;
   }
 
+// struct Comment
   struct Comment {
     uint256 postId;
     string commentBody;
@@ -40,12 +45,14 @@ contract PostIt {
   uint256 commentId;
   address public owner;
 
+  // Event logging
   event LogEnrolled(address accountAddress);
   event LogNewPostAdded(uint256 postId, string title, string fullName, address accountAddress);
   event LogUpVote(uint256 postId, string title, uint votes, string fullName, address accountAddress);
   event LogDownVote(uint256 postId, string title, uint votes, string fullName, address accountAddress);
   event LogComment(uint256 postId, uint256 commentId, string commentBody, string fullName, address accountAddress);
 
+  // modifiers
   modifier onlyOwner () { require (msg.sender == owner, "You are not the owner."); _;}
   modifier notPostAuthor (uint256 _postId) { require (posts[_postId].accountAddress != msg.sender, "You are the post author."); _;}
   modifier notEnrolled () { require (enrolled[msg.sender] != true, "You are already enrolled with this address."); _;}
@@ -56,7 +63,12 @@ contract PostIt {
     commentId = 1;
   }
 
-  //Add a check to make sure the address is not bound aleady
+  /** @dev Enrolls a user profile and their account address.
+    * @param firstName of the User.
+    * @param lastName of the User.
+    * @param email of the User.
+    * @return a bool.
+    */
   function enroll(string memory firstName, string memory lastName, string memory email) notEnrolled() public returns (bool) {
     enrolled[msg.sender] = true;
     userProfiles[msg.sender] = User({firstName: firstName, lastName: lastName, email: email, accountAddress: msg.sender});
@@ -64,10 +76,19 @@ contract PostIt {
     return enrolled[msg.sender];
   }
 
+  /** @dev Get a user profile and their account address.
+    * @return a array of firstName, lastName and email of a User.
+    */
   function getUserProfile() public view returns (string memory, string memory, string memory, address) {
     return (userProfiles[msg.sender].firstName, userProfiles[msg.sender].lastName, userProfiles[msg.sender].email, userProfiles[msg.sender].accountAddress);
   }
 
+  /** @dev Add a post.
+    * @param title of a Post.
+    * @param content of a Post.
+    * @param default vote of a Post.
+    * @return a bool.
+    */
   function addPostIt(string memory title, string memory content, uint vote) public returns(bool) {
     posts[postId] = Post({title: title, content: content, votes: vote, author: string(abi.encodePacked(userProfiles[msg.sender].firstName, " ", userProfiles[msg.sender].lastName)),  accountAddress: msg.sender});
     emit LogNewPostAdded(postId, title, string(abi.encodePacked(userProfiles[msg.sender].firstName, " ", userProfiles[msg.sender].lastName)), msg.sender);
@@ -76,10 +97,18 @@ contract PostIt {
     return true;
   }
 
+  /** @dev Get a post.
+    * @param postId of a Post.
+    * @return a array with postId, title, content, votes, author, accountAddress (of author).
+    */
   function getPost(uint256 _postId) public view returns(uint256, string memory, string memory, uint, string memory, address) {
     return(_postId, posts[_postId].title, posts[_postId].content, posts[_postId].votes, posts[_postId].author, posts[_postId].accountAddress);
   }
 
+  /** @dev Vote up a post, if you're not the author.
+    * @param postId of a Post.
+    * @return a address of the user.
+    */
   function makeUpVote(uint256 _postId) notPostAuthor(_postId) public returns(address) {
     require(posts[_postId].upVoters[msg.sender].accountAddress != msg.sender, "user has already up voted.");
     posts[_postId].upVoters[msg.sender] = userProfiles[msg.sender];
@@ -89,6 +118,10 @@ contract PostIt {
     return msg.sender;
   }
 
+  /** @dev Vote down a post, if you're not the author.
+    * @param postId of a Post.
+    * @return a address of the user.
+    */
   function makeDownVote(uint256 _postId) notPostAuthor(_postId) public returns(address) {
     require(posts[_postId].downVoters[msg.sender].accountAddress != msg.sender, "user has already up voted.");
     posts[_postId].upVoters[msg.sender] = userProfiles[msg.sender];
@@ -98,6 +131,11 @@ contract PostIt {
     return msg.sender;
   }
 
+  /** @dev Add a comment to a post.
+    * @param postId of a Post.
+    * @param commentBody of the comment.
+    * @return a bool.
+    */
   function addComment(uint256 _postId, string memory commentBody) public returns(bool) {
     posts[postId].comments[commentId] = Comment({postId: _postId, commentBody: commentBody});
     posts[postId].comments[commentId].commenters[msg.sender] = userProfiles[msg.sender];
@@ -107,22 +145,36 @@ contract PostIt {
     return true;
   }
 
+  /** @dev Get all post indexed.
+    * @return a array of postIndexes.
+    */
   function getAllPosts() external view returns(uint256[] memory) {
     return postsIndexes;
   }
 
+  /** @dev Get all up voters indexed.
+    * @return a array of upVotersIndexes.
+    */
   function getAllUpVoters() external view returns(address[] memory) {
     return upVotersIndexes;
   }
 
+  /** @dev Get all down voters indexed.
+    * @return a array of downVotersIndexes.
+    */
   function getAllDownVoters() external view returns(address[] memory) {
     return downVotersIndexes;
   }
 
+  /** @dev Get all comments indexed.
+    * @return a array of commentsIndexes.
+    */
   function getAllComments() external view returns(uint256[] memory) {
     return commentsIndexes;
   }
 
+  /** @dev Fallback revert method.
+    */
   function() external {
     revert();
   }
